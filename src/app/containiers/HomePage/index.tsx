08e82189575a2616753a6c05a668f89eb7f0c6dc
MD ZIAUDDIN <md.ziauddin02@gmail.com>
2022-10-29 14:40:44 +0530

    Adds About us component

--- src/app/containiers/HomePage/index.tsx
+++ src/app/containiers/HomePage/index.tsx
@@ -4,6 +4,7 @@ import tw from "twin.macro";
 import { BookCard } from "../../components/BookCard";
 import { Marginer } from "../../components/marginer";
 import { Navbar } from "../../components/navbar";
+import { AboutUs } from "./aboutUs";
 import { BookingSteps } from "./bookingSteps";
 import { TopSection } from "./topSection";
 
@@ -27,6 +28,8 @@ export const HomePage = () => {
       <BookCard />
       <Marginer direction="vertical" margin="10em" />
       <BookingSteps />
+      <Marginer direction="vertical" margin="8em" />
+      <AboutUs /> 
     </PageConatiner>
   );
 };
